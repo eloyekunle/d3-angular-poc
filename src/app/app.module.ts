14795c3b726660963043e12e867f868834800a41
Elijah Oyekunle <eloyekunle@gmail.com>
2019-03-11 22:37:09 +0100

    added generic chart module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { DonutComponent } from './donut/donut.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BarComponent } from './bar/bar.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DonutComponent,
     HomeComponent,
-    BarComponent
+    BarComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'donut', component: DonutComponent},
       {path: 'bar', component: BarComponent},
+      {path: 'chart', component: ChartComponent},
       {path: '', component: HomeComponent},
     ])
   ],
--- src/app/donut/donut.component.ts
+++ src/app/donut/donut.component.ts
@@ -25,9 +25,7 @@ export class DonutComponent implements OnInit {
       .innerRadius(radius - 100)
       .outerRadius(radius - 20);
 
-    const svg = d3.select('body').append('svg')
-      .attr('width', width)
-      .attr('height', height)
+    const svg = d3.select('svg')
       .append('g')
       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
