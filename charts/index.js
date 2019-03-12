d5ec12706440ebf8fbac7549ea76e71f185b2c30
Elijah Oyekunle <eloyekunle@gmail.com>
2019-03-12 01:16:23 +0100

    chart library wip, installed @types/d3

---
+++ charts/index.js
@@ -0,0 +1,9 @@
+const kdc = {};
+kdc.models = kdc.models || {};
+
+// Eventually moving to its own file..
+kdc.models.pieChart = function() {
+  return {};
+}
+
+export default kdc;
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {select, Selection} from 'd3';
+import Chart from '../../../charts';
+
+interface ChartData {
+  value: number;
+}
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
+  id = 'chart';
+  size = 300;
+
   constructor() { }
 
+  initPieChart(svg: Selection<any, any, any, any>, data: ChartData[]) {
+    const chart = Chart.models.pieChart();
+    const size = this.size || 280;
+
+    svg.attr('height', size)
+      .attr('width', size)
+      .append('g')
+      .datum(data)
+      .transition()
+      .duration(350)
+      .call(chart);
+  }
+
   ngOnInit() {
+    const svg = select(`#${this.id}`).append('svg');
+    const chart = this.initPieChart(svg, [{value: 10}, {value: 25}]);
   }
 
 }
