0ab412ce53dd175d410cc4f2bd60de782afa9253
Elijah Oyekunle <eloyekunle@gmail.com>
2019-03-12 10:11:05 +0100

    added more option methods

--- charts/index.js
+++ charts/index.js
@@ -4,7 +4,11 @@ kdc.models = kdc.models || {};
 // Each chart type will move to their own file..
 kdc.models.pieChart = function() {
   let showLegend = false,
-      showLabels = true;
+      showLabels = true,
+      donutRatio = 0.5,
+      getX = function(d) { return d.x },
+      getY = function(d) { return d.y },
+      donut = false;
 
   function chart(selection) {
     console.log(selection);
@@ -12,7 +16,11 @@ kdc.models.pieChart = function() {
 
   chart._options = Object.create({}, {
     showLegend: { get: function(){return showLegend;}, set: function(_){showLegend=_;}},
-    showLabels: { get: function(){return showLabels;}, set: function(_){showLabels=_;}}
+    showLabels: { get: function(){return showLabels;}, set: function(_){showLabels=_;}},
+    donutRatio: { get: function(){return donutRatio;}, set: function(_){donutRatio=_;}},
+    x:          { get: function(){return getX;}, set: function(_){getX=_;}},
+    y:          { get: function(){return getY;}, set: function(_){getY=_;}},
+    donut:      { get: function(){return donut;}, set: function(_){donut=_;}},
   })
 
   initOptions(chart);
